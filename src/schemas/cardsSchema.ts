efb41e838925df471ffe6c7bf3486f7ed0c45f4d
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-09-11 20:38:46 -0300

    [FIX]ajustes de validacao de dados de cartao

--- src/schemas/cardsSchema.ts
+++ src/schemas/cardsSchema.ts
@@ -1,12 +1,14 @@
-import joi from 'joi';
+import Joi from 'joi';
+import JoiDate from '@joi/date';
+const joi = Joi.extend(JoiDate)
 
 export const newCardSchema = joi.object({
     title:joi.string().trim().required(),
-    cardNumber:joi.string().trim().regex(/[0-9]/).required(),
+    cardNumber:joi.string().trim().regex(/[ˆ0-9]/).required(),
     holderName:joi.string().trim().required(),
-    securityCode:joi.string().trim().required().max(3),
-    expirationDate:joi.string().required(),
+    securityCode:joi.string().trim().required().length(3).regex(/[0-9]{3}/),
+    expirationDate:joi.date().format('MM/YY'),
     password:joi.string().trim().required(),
     isVirtual:joi.boolean().required(),
     type:joi.string().trim().valid('CREDIT','DEBT','BOTH').required()
-})
+}) 
